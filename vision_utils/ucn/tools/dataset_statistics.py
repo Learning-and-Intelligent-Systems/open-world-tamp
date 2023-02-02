1b50e4b72b5a3097904781c50d65fe349d5ae3ee
Aidan Curtis <aidancurtis@Aidans-MacBook-Pro.local>
2023-02-02 11:45:03 -0500

    ucn lib

--- vision_utils/ucn/tools/dataset_statistics.py
+++ vision_utils/ucn/tools/dataset_statistics.py
@@ -2,35 +2,34 @@
 # This work is licensed under the NVIDIA Source Code License - Non-commercial. Full
 # text can be found in LICENSE.md
 
-import glob
-import math
 import os
 import os.path
-import random
-
-import _init_paths
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
 import torch
-from datasets import OCIDObject, OSDObject
+import cv2
+import numpy as np
+import glob
+import random
+import math
 from transforms3d.quaternions import mat2quat, quat2mat
+import _init_paths
+from datasets import OCIDObject, OSDObject
+import matplotlib.pyplot as plt
 from utils import mask as util_
 
-if __name__ == "__main__":
-    dataset = OSDObject("test")
+if __name__ == '__main__':
+    dataset = OSDObject('test')
     num = dataset._size
     num_objects = []
     for i in range(num):
 
         filename = str(dataset.image_files[i])
         # labels_filename = filename.replace('rgb', 'label')
-        labels_filename = filename.replace("image_color", "annotation")
+        labels_filename = filename.replace('image_color', 'annotation')
 
         foreground_labels = util_.imread_indexed(labels_filename)
         # mask table as background
         foreground_labels[foreground_labels == 1] = 0
-        if "table" in labels_filename:
+        if 'table' in labels_filename:
             foreground_labels[foreground_labels == 2] = 0
         foreground_labels = dataset.process_label(foreground_labels)
         n = len(np.unique(foreground_labels)) - 1
@@ -38,6 +37,6 @@ if __name__ == "__main__":
         print(labels_filename, n)
 
     nums = np.array(num_objects)
-    print("min: %d" % (np.min(nums)))
-    print("max: %d" % (np.max(nums)))
-    print("mean: %f" % (np.mean(nums)))
+    print('min: %d' % (np.min(nums)))
+    print('max: %d' % (np.max(nums)))
+    print('mean: %f' % (np.mean(nums)))
--- vision_utils/ucn/tools/test_net.py
+++ vision_utils/ucn/tools/test_net.py
@@ -6,72 +6,49 @@
 
 """Test a DeepIM network on an image database."""
 
+import torch
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.utils.data
+
 import argparse
-import os
-import os.path as osp
 import pprint
+import time, os, sys
+import os.path as osp
+import numpy as np
 import random
-import sys
-import time
+import scipy.io
 
 import _init_paths
-import networks
-import numpy as np
-import scipy.io
-import torch
-import torch.backends.cudnn as cudnn
-import torch.nn.parallel
-import torch.utils.data
-from datasets.factory import get_dataset
-from fcn.config import cfg, cfg_from_file, get_output_dir
 from fcn.test_dataset import test_segnet
-
+from fcn.config import cfg, cfg_from_file, get_output_dir
+from datasets.factory import get_dataset
+import networks
 
 def parse_args():
     """
     Parse input arguments
     """
-    parser = argparse.ArgumentParser(description="Test a Unseen Clustering Network")
-    parser.add_argument(
-        "--gpu", dest="gpu_id", help="GPU id to use", default=0, type=int
-    )
-    parser.add_argument(
-        "--pretrained",
-        dest="pretrained",
-        help="initialize with pretrained checkpoint",
-        default=None,
-        type=str,
-    )
-    parser.add_argument(
-        "--pretrained_crop",
-        dest="pretrained_crop",
-        help="initialize with pretrained checkpoint for crops",
-        default=None,
-        type=str,
-    )
-    parser.add_argument(
-        "--cfg", dest="cfg_file", help="optional config file", default=None, type=str
-    )
-    parser.add_argument(
-        "--dataset",
-        dest="dataset_name",
-        help="dataset to train on",
-        default="shapenet_scene_train",
-        type=str,
-    )
-    parser.add_argument(
-        "--rand",
-        dest="randomize",
-        help="randomize (do not use a fixed seed)",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--network",
-        dest="network_name",
-        help="name of the network",
-        default=None,
-        type=str,
-    )
+    parser = argparse.ArgumentParser(description='Test a Unseen Clustering Network')
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU id to use',
+                        default=0, type=int)
+    parser.add_argument('--pretrained', dest='pretrained',
+                        help='initialize with pretrained checkpoint',
+                        default=None, type=str)
+    parser.add_argument('--pretrained_crop', dest='pretrained_crop',
+                        help='initialize with pretrained checkpoint for crops',
+                        default=None, type=str)
+    parser.add_argument('--cfg', dest='cfg_file',
+                        help='optional config file', default=None, type=str)
+    parser.add_argument('--dataset', dest='dataset_name',
+                        help='dataset to train on',
+                        default='shapenet_scene_train', type=str)
+    parser.add_argument('--rand', dest='randomize',
+                        help='randomize (do not use a fixed seed)',
+                        action='store_true')
+    parser.add_argument('--network', dest='network_name',
+                        help='name of the network',
+                        default=None, type=str)
 
     if len(sys.argv) == 1:
         parser.print_help()
@@ -80,11 +57,10 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     args = parse_args()
 
-    print("Called with args:")
+    print('Called with args:')
     print(args)
 
     if args.cfg_file is not None:
@@ -92,7 +68,7 @@ if __name__ == "__main__":
 
     if len(cfg.TEST.CLASSES) == 0:
         cfg.TEST.CLASSES = cfg.TRAIN.CLASSES
-    print("Using config:")
+    print('Using config:')
     pprint.pprint(cfg)
 
     if not args.randomize:
@@ -101,8 +77,8 @@ if __name__ == "__main__":
 
     # device
     cfg.gpu_id = 0
-    cfg.device = torch.device("cuda:{:d}".format(cfg.gpu_id))
-    print("GPU device {:d}".format(args.gpu_id))
+    cfg.device = torch.device('cuda:{:d}'.format(cfg.gpu_id))
+    print('GPU device {:d}'.format(args.gpu_id))
 
     # prepare dataset
     if cfg.TEST.VISUALIZE:
@@ -110,20 +86,13 @@ if __name__ == "__main__":
         np.random.seed()
     else:
         shuffle = False
-    cfg.MODE = "TEST"
+    cfg.MODE = 'TEST'
     dataset = get_dataset(args.dataset_name)
-    worker_init_fn = (
-        dataset.worker_init_fn if hasattr(dataset, "worker_init_fn") else None
-    )
+    worker_init_fn = dataset.worker_init_fn if hasattr(dataset, 'worker_init_fn') else None
     num_workers = 1
-    dataloader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=cfg.TEST.IMS_PER_BATCH,
-        shuffle=shuffle,
-        num_workers=num_workers,
-        worker_init_fn=worker_init_fn,
-    )
-    print("Use dataset `{:s}` for training".format(dataset.name))
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=cfg.TEST.IMS_PER_BATCH, shuffle=shuffle,
+        num_workers=num_workers, worker_init_fn=worker_init_fn)
+    print('Use dataset `{:s}` for training'.format(dataset.name))
 
     # overwrite intrinsics
     if len(cfg.INTRINSICS) > 0:
@@ -132,37 +101,29 @@ if __name__ == "__main__":
         print(dataset._intrinsic_matrix)
 
     output_dir = get_output_dir(dataset, None)
-    print("Output will be saved to `{:s}`".format(output_dir))
+    print('Output will be saved to `{:s}`'.format(output_dir))
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     # prepare network
     if args.pretrained:
         network_data = torch.load(args.pretrained)
-        if isinstance(network_data, dict) and "model" in network_data:
-            network_data = network_data["model"]
+        if isinstance(network_data, dict) and 'model' in network_data:
+            network_data = network_data['model']
         print("=> using pre-trained network '{}'".format(args.pretrained))
     else:
         network_data = None
         print("no pretrained network specified")
         sys.exit()
 
-    network = networks.__dict__[args.network_name](
-        dataset.num_classes, cfg.TRAIN.NUM_UNITS, network_data
-    ).cuda(device=cfg.device)
-    network = torch.nn.DataParallel(network, device_ids=[cfg.gpu_id]).cuda(
-        device=cfg.device
-    )
+    network = networks.__dict__[args.network_name](dataset.num_classes, cfg.TRAIN.NUM_UNITS, network_data).cuda(device=cfg.device)
+    network = torch.nn.DataParallel(network, device_ids=[cfg.gpu_id]).cuda(device=cfg.device)
     cudnn.benchmark = True
 
     if args.pretrained_crop:
         network_data_crop = torch.load(args.pretrained_crop)
-        network_crop = networks.__dict__[args.network_name](
-            dataset.num_classes, cfg.TRAIN.NUM_UNITS, network_data_crop
-        ).cuda(device=cfg.device)
-        network_crop = torch.nn.DataParallel(
-            network_crop, device_ids=[cfg.gpu_id]
-        ).cuda(device=cfg.device)
+        network_crop = networks.__dict__[args.network_name](dataset.num_classes, cfg.TRAIN.NUM_UNITS, network_data_crop).cuda(device=cfg.device)
+        network_crop = torch.nn.DataParallel(network_crop, device_ids=[cfg.gpu_id]).cuda(device=cfg.device)
     else:
         network_crop = None
 
