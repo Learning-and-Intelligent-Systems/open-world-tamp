6a84bc52f5ae8537bb5d74f1f5d981abb2a2d5f2
Aidan Curtis <curtisa@mit.edu>
2023-02-06 17:37:39 -0500

    comment out exploration tests temporarily

--- tests/test_exploration.py
+++ tests/test_exploration.py
@@ -1,12 +1,13 @@
 from run_planner import create_parser, main
 import pytest
 
-def test_run_planner_lamb():
-    parser = create_parser()
-    args = parser.parse_args(["--robot", "movo", "--goal", "all_green", "--world", "vanamo_m0m_chair", "--exploration", "--base-planner", "lamb"])
-    main(args)
-
 # This one takes a long time
+
+# def test_run_planner_lamb():
+#     parser = create_parser()
+#     args = parser.parse_args(["--robot", "movo", "--goal", "all_green", "--world", "vanamo_m0m_chair", "--exploration", "--base-planner", "lamb"])
+#     main(args)
+
 # def test_run_planner_astar():
 #     parser = create_parser()
 #     args = parser.parse_args(["--robot", "movo", "--goal", "all_green", "--world", "vanamo_m0m", "--exploration", "--base-planner", "astar"])
